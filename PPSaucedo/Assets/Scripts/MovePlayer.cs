e212691446d961d828b7adebedfd8406e879fede
MathSauc3 <732584@student.dusd.net>
2023-05-08 00:09:14 -0700

    NeedTofixFallingCamera
    NoMovement yet and no new models need to work on camera cause it keeps falling through the floor for some forsaken reason.

--- PPSaucedo/Assets/Scripts/MovePlayer.cs
+++ PPSaucedo/Assets/Scripts/MovePlayer.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+	private Vector2 PlayerMouseInput;
+	public Transform PlayerCamera;
 	public Rigidbody PlayerBody;
 	private Vector3 PlayerMovementInput;
+	private float xRot;
+	private float yRot;
 
 	public float Speed;
 	public float JumpForce;
+	public float Sensitivity;
+
+	private void start()
+	{
+
+	}
+
 
 	private void Update()
 	{
 		PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+		PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 		movePlayer();
+		MovePlayerCamera();
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	private void movePlayer()
@@ -29,6 +44,11 @@ public class MovePlayer : MonoBehaviour
 
 	private void MovePlayerCamera()
 	{
+		xRot -= PlayerMouseInput.y * Sensitivity;
+		xRot = Mathf.Clamp(xRot, -90f, 90f);
+		transform.Rotate(0f, PlayerMouseInput.x *Sensitivity, 0f);
+		PlayerCamera.transform.localRotation = Quaternion.Euler (xRot, 0f, 0f);
+
 
 	}
 }
