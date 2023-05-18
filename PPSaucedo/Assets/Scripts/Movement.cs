280ddb2b4b99d21fe530f6dde09ef6a80e4757a2
MathSauc3 <732584@student.dusd.net>
2023-05-18 11:40:57 -0700

    Try add jumping no guns yet

--- PPSaucedo/Assets/Scripts/Movement.cs
+++ PPSaucedo/Assets/Scripts/Movement.cs
@@ -7,6 +7,19 @@ public class Movement : MonoBehaviour
     [Header ("Movement")]
     public float moveSpeed;
 
+    public float groundDrag;
+
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airmultiplier;
+    bool readyToJump;
+    public KeyCode jumpKey = KeyCode.Space;
+
+
     public Transform orientation;
 
     float horizontalInput;
@@ -32,17 +45,52 @@ public class Movement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey)&& readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+       if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+       else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airmultiplier, ForceMode.Force);
     }
 
     void Update()
     {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
         MyInput();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+
     }
 }
